271152b25fce9e120e04f5e50fcd6482a0a6bb5a
med134 <81263260+med134@users.noreply.github.com>
2023-12-16 19:06:13 +0100

    helo

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: "https",
+            hostname: "i.ibb.co",
+          },
+          {
+            protocol: "https",
+            hostname: "res.cloudinary.com",
+          },
+          {
+            protocol: "https",
+            hostname: "avatars.githubusercontent.com",
+          },
+        ],
+        minimumCacheTTL: 1500000,
+      },
+}
 
 module.exports = nextConfig
