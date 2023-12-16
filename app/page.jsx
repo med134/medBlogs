1692efa8c1086036f15b7ca821e70fad70d657c7
med134 <81263260+med134@users.noreply.github.com>
2023-12-16 18:54:00 +0100

    sd

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Hero from "./components/Hero";
 
 export default async function Home() {
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>hello world z</h1>
+      <h1>hello world</h1>
+
     </main>
   );
 }
