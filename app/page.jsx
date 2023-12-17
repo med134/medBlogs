76b2bc86e408177fab26a1b61cec3f5d2d4c0263
med134 <81263260+med134@users.noreply.github.com>
2023-12-17 01:00:11 +0100

    grt

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Link from 'next/link';
 async function getData() {
-  const res = await fetch(`https://www.medcode.dev/api/posts`, {
+  const res = await fetch(`http://localhost:3000/api/posts`, {
     cache: "no-store",
   });
   if (!res.ok) {
@@ -12,7 +12,6 @@ async function getData() {
 }
 export default async function Home() {
   const data = await getData();
-  console.log(data);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>hello world</h1>
