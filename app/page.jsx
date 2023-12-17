c44b774a5ab40e1fc5bbafb86dfe6a3898d96e52
med134 <81263260+med134@users.noreply.github.com>
2023-12-17 19:17:30 +0100

    refresh

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 
-
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>hello world</h1>
       <article className="grid grid-cols-3 gap-6 p-16 xl:gap-4 xl:p-8 lg:grid-cols-2 lg:gap-6 lg:p-10 sm:flex flex-wrap dark:bg-dark">
-        <h4>hey</h4>
-        </article>
+        <h4>Next js app</h4>
+      </article>
     </main>
   );
 }
