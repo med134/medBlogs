1946c178af71d7528ee10f42fe50610838676695
med134 <81263260+med134@users.noreply.github.com>
2023-12-17 01:13:30 +0100

    ef

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import NavBar from "./components/NavBar";
 const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Software Mastery-Insider Dev Guides-Blogs | medCode",
@@ -12,7 +11,6 @@ export default function RootLayout({ children }) {
   return (
       <html lang="en">
         <body className={inter.className} suppressHydrationWarning={true}>
-          <NavBar />
           {children}
         </body>
       </html>
