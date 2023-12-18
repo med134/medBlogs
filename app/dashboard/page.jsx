266fa51e6c0589f6f666f03e9094c07fe048a4df
med134 <81263260+med134@users.noreply.github.com>
2023-12-18 23:32:26 +0100

    adding dashboard

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,10 @@
 
 import React from "react";
+import NavigationBar from "../components/NavigationBar";
 export default function DashBoard() {
   return (
     <div>
-      <h1>dashboard</h1>
+      <NavigationBar/>
     </div>
   );
 }
