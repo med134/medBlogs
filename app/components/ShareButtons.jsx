76e048a9ed6b1e8c83f05e74db5e5d2f071ac753
med134 <81263260+med134@users.noreply.github.com>
2024-01-04 01:08:50 +0100

    share on social media

--- app/components/ShareButtons.jsx
+++ app/components/ShareButtons.jsx
@@ -9,8 +9,8 @@ import {
 
 const ShareButtons = ({ url }) => {
   return (
-    <div className="flex justify-start items-center mb-4">
-      <span className="font-semibold text-xl text-dark dark:text-light">
+    <div className="flex justify-start items-center mb-4 sm:block">
+      <span className="font-semibold text-xl text-dark dark:text-light sm:px-4 sm:mb-2">
         Share on Social media
       </span>
       <div className="ml-3">
@@ -68,22 +68,18 @@ const ShareButtons = ({ url }) => {
             </svg>
           </button>
         </WhatsappShareButton>
-        <TelegramShareButton url={url} className="ml-2">
+        <TelegramShareButton url={url} className="ml-4">
           <svg
+            className="h-7 w-7"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            style={{ color: "#0088cc" }}
+            version="1.1"
             xmlns="http://www.w3.org/2000/svg"
-            aria-label="Telegram"
-            role="img"
-            viewBox="0 0 512 512"
           >
-            <rect width={24} height={24} rx="15%" fill="#37aee2" />
             <path
-              fill="#c8daea"
-              d="M199 404c-11 0-10-4-13-14l-32-105 245-144"
-            />
-            <path fill="#a9c9dd" d="M199 404c7 0 11-4 16-8l45-43-56-34" />
-            <path
-              fill="#f6fbfe"
-              d="M204 319l135 99c14 9 26 4 30-14l55-258c5-22-9-32-24-25L79 245c-21 8-21 21-4 26l83 26 190-121c9-5 17-3 11 4"
+              id="telegram-1"
+              d="M18.384,22.779c0.322,0.228 0.737,0.285 1.107,0.145c0.37,-0.141 0.642,-0.457 0.724,-0.84c0.869,-4.084 2.977,-14.421 3.768,-18.136c0.06,-0.28 -0.04,-0.571 -0.26,-0.758c-0.22,-0.187 -0.525,-0.241 -0.797,-0.14c-4.193,1.552 -17.106,6.397 -22.384,8.35c-0.335,0.124 -0.553,0.446 -0.542,0.799c0.012,0.354 0.25,0.661 0.593,0.764c2.367,0.708 5.474,1.693 5.474,1.693c0,0 1.452,4.385 2.209,6.615c0.095,0.28 0.314,0.5 0.603,0.576c0.288,0.075 0.596,-0.004 0.811,-0.207c1.216,-1.148 3.096,-2.923 3.096,-2.923c0,0 3.572,2.619 5.598,4.062Zm-11.01,-8.677l1.679,5.538l0.373,-3.507c0,0 6.487,-5.851 10.185,-9.186c0.108,-0.098 0.123,-0.262 0.033,-0.377c-0.089,-0.115 -0.253,-0.142 -0.376,-0.064c-4.286,2.737 -11.894,7.596 -11.894,7.596Z"
             />
           </svg>
         </TelegramShareButton>
