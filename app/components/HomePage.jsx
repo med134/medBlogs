a28f89c6c1ebf477fbc33b44dc1ab1fa8fc70500
med134 <81263260+med134@users.noreply.github.com>
2024-01-29 17:20:48 +0100

    add margin top

--- app/components/HomePage.jsx
+++ app/components/HomePage.jsx
@@ -20,7 +20,7 @@ export default function HomePage() {
       });
   }, []);
   return (
-    <section className="">
+    <section className="mt-4">
       <span className="text-2xl underline dark:text-light bg-light dark:bg-dark font-bold flex justify-start items-center text-gray-800 font-slab px-12 xs:px-8 pt-8 py-2 xs:pt-1">
         <AiTwotoneSound className="dark:text-light" />
         <p className="ml-2 sm:text-xl">Recent Articles</p>
