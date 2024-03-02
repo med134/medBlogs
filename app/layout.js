c0958ab897898e25364d62f4ded6f16ed1e1c247
med134 <81263260+med134@users.noreply.github.com>
2024-03-02 12:51:09 +0100

    add vercel

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import dynamic from "next/dynamic";
+import { Analytics } from "@vercel/analytics/react";
 const NavBar = dynamic(() => import("./components/NavBar"));
 const Footer = dynamic(() => import("./components/Footer"));
 import AuthProvider from "./components/authProvider/AuthProvider";
@@ -84,6 +85,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <GoogleAnalytic />
+      <Analytics />
       <body className={inter.className} suppressHydrationWarning={true}>
         <AuthProvider>
           <NavBar />
