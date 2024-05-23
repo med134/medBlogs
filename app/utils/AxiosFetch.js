35152a41552b6a80ff88f8f75d931926faa92e3e
med134 <81263260+med134@users.noreply.github.com>
2024-05-23 20:13:45 +0100

    add some changes

--- app/utils/AxiosFetch.js
+++ app/utils/AxiosFetch.js
@@ -13,7 +13,7 @@ const getBlog = async () => axiosClient.get(`/blog`);
 const getBlogBySlug = async (slug) => {
   const response = await fetch(`https://www.medcode.dev/api/blog/${slug}`);
   const data = await response.json();
-  console.log("single post", data);
+  console.log("single post",data)
   return data;
 };
 
