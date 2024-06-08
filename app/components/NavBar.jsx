feb2ff257ea2b22aeb50a24b28c075299cca9d12
med134 <81263260+med134@users.noreply.github.com>
2024-06-08 19:43:28 +0100

    remove search

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -79,42 +79,6 @@ const NavBar = () => {
     >
       <div className="items-center hidden lg:inline-flex ml-[94%] xs:ml-[85%] p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg  dark:text-gray-400 ">
         <button
-          type="button"
-          id="bt-search"
-          aria-label="search-button-label"
-          data-collapse-toggle="navbar-search"
-          onClick={handleClick}
-          className="hover:bg-gray-100 dark:text-light rounded-lg focus:outline-none p-2 focus:ring-2 focus:ring-gray-200 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-        >
-          <span id="search-button-label" className="sr-only">
-            Search Button
-          </span>
-          <svg
-            width="24px"
-            height="24px"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className=""
-          >
-            <g id="SVGRepo_bgCarrier" strokeWidth={0} />
-            <g
-              id="SVGRepo_tracerCarrier"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <g id="SVGRepo_iconCarrier">
-              <path
-                d="M16.6725 16.6412L21 21M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z"
-                stroke="currentColor"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </g>
-          </svg>
-        </button>
-        <button
           data-collapse-toggle="navbar-search"
           aria-labelledby="menu-button"
           type="button"
