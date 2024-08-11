bf58dc5f8be66bcc7d64b2878b35ecd832f1eb42
med134 <81263260+med134@users.noreply.github.com>
2024-08-11 18:35:41 +0100

    add

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,18 +1,9 @@
-"use client";
-import React, { useEffect } from "react";
+import React from "react";
 import ChartPage from "../components/ChartPage";
 import HomeDashboard from "../components/HomeDashboard";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+
 
 const page = () => {
-  const session = useSession();
-  const router = useRouter();
-  useEffect(() => {
-    if (session.status != "authenticated") {
-      router.push(`/dashboard/login`);
-    }
-  }, [session, router]);
   return (
     <div className="p-12 w-[100%] bg-white dark:bg-dark sm:p-4 col-auto pt-20 px-1">
       <h1 className="text-2xl mt-4 flex text-dark font-semibold">
