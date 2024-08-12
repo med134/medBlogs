c4066e9038876436aeeb3d405e18c3b4bff87ea8
med134 <81263260+med134@users.noreply.github.com>
2024-08-12 21:31:34 +0100

    add reload

--- app/components/UsersDashboard.jsx
+++ app/components/UsersDashboard.jsx
@@ -34,7 +34,7 @@ const UsersDashboard = () => {
         await fetch(`/api/users/${id}`, {
           method: "DELETE",
         });
-        router.refresh();
+        router.reload();
       } catch (err) {
         console.log(err);
       }
