1b5902c57e88f6116f1490648c5486c16b7f94ea
med134 <81263260+med134@users.noreply.github.com>
2024-08-28 22:38:46 +0100

    add middleware

--- app/dashboard/blogs/page.jsx
+++ app/dashboard/blogs/page.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ListDashboardBlogs from "@/app/components/ListDashboardBlogs";
-import { getPostsByUser,getPosts } from "@/app/utils/action";
+import { getPostsByUser } from "@/app/utils/action";
 import { auth } from "@/app/utils/auth";
 
 const Blogs = async () => {
   const session = await auth();
-  const allPosts =await getPosts();
   const posts = await getPostsByUser(session?.user.name);
+  console.log(posts)
   return (
     <div className="mx-auto p-6 pt-24 h-full">
       <ListDashboardBlogs data={posts}/>
--- app/utils/auth.config.js
+++ app/utils/auth.config.js
@@ -11,6 +11,7 @@ export const authConfig = {
         token.userSlug = user.userSlug;
       }
       return token;
+      console.log(token);
     },
     async session({ session, token }) {
       if (token) {
@@ -19,25 +20,19 @@ export const authConfig = {
         session.user.userSlug = token.userSlug;
       }
       return session;
+      console.log(session);
     },
     authorized({ auth, request }) {
       const user = auth?.user;
-      console.log(user,auth);
       const isOneBlog = request?.nextUrl?.pathname?.startsWith("/dashboard");
       const isOnLoginPage = request?.nextUrl?.pathname.startsWith("/login");
-      const adminUserPage =
+      const adminPages =
+        request?.nextUrl?.pathname.startsWith("/dashboard/pending") ||
         request?.nextUrl?.pathname.startsWith("/dashboard/users");
 
-      const adminDraftBlog =
-        request?.nextUrl?.pathname.startsWith("dashboard/pending");
-
-      if (adminUserPage && user?.name != "MOHAMMED DAKIR") {
+      if (adminPages && user?.name != "MOHAMMED DAKIR") {
         return Response.redirect(new URL("/dashboard", request.nextUrl));
       }
-      if (adminDraftBlog && user?.name != "MOHAMMED DAKIR") {
-        return Response.redirect(new URL("/dashboard", request.nextUrl));
-      }
-
       if (isOneBlog && !user) {
         return Response.redirect(new URL("/login", request.nextUrl));
       }
