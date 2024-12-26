8170ba73dcac5810900ae8e9fcde11ab6d9bc8eb
med134 <81263260+med134@users.noreply.github.com>
2024-12-26 21:54:32 +0100

    add login

--- middleware.js
+++ middleware.js
@@ -1,8 +1,16 @@
-import NextAuth from "next-auth";
-import { authConfig } from "@/auth.config";
-
-export default NextAuth(authConfig).auth;
+import { NextResponse } from "next/server";
+import { auth } from "./auth";
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
+
+export default auth((req) => {
+  console.log("come from auth", req.auth);
+  // If the user is authenticated and trying to access the login page, redirect to home page
+  if (req.auth && req.nextUrl.pathname === "/login") {
+    const newUrl = new URL("/dashboard", req.nextUrl.origin);
+    return NextResponse.redirect(newUrl);
+  }
+  return NextResponse.next();
+});
