87f5614366c701a68c74b3810a2d84c6b3e5588d
med134 <81263260+med134@users.noreply.github.com>
2024-12-29 15:30:39 +0100

    react

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -58,7 +58,7 @@ const page = () => {
               <p className="font-lexend text-xl">
                 Hi, I&apos;m med, a web developer and UI/UX designer with a
                 passion for creating beautiful, functional, and user-centered
-                digital experiences. With 3 years of experience in the field. I
+                digital experiences. With 4 years of experience in the field. I
                 am always looking for new and innovative ways to bring my
                 clients&apos; visions to life.
               </p>
